567ba10e15fca77b0bca8f5a70161e997d520bd0
Connor-Berkman <bcr346140@gmail.com>
2022-10-26 09:58:59 -0300

    First Trigger

---
+++ Assets/Scrips/AnimatedTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTrigger : MonoBehaviour
+{
+    public GameObject AnimatedObject;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = AnimatedObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _animator.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _animator.enabled = false;
+    }
+
+}
